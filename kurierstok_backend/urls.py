eb840897117fbcfe03cf792da36eb6617a4cf024
kmichalski695 <michalski695@gmail.com>
2021-07-10 19:51:49 +0200

    rest api i podstawowe modele

--- kurierstok_backend/urls.py
+++ kurierstok_backend/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include        # add this
-from django.conf import settings             # add this
-from django.conf.urls.static import static   # add this
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
